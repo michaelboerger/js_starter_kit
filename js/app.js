0dcfee670a608f7443a1360916c3280c75bb88db
Michael <michael.boerger@gmail.com>
2014-02-28 12:08:13 -0500

    Added random number sort of

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var randomnum = Math.random();
 var name = prompt('HELLO HUMAN FRIEND, WHAT IS YOUR NAME');
 var num = prompt('HELLO HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER');
 alert('YOU PICKED ' + num + ' COMPUTERS ARE SO USEFUL');
+alert('MY NUMBER WAS ' + randomnum + ' I AM NOT PROGRAMMED TO CARE IF YOU GUESSED IT');
