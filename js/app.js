0c30add992327591409ea39bc8de925b3700370d
Michael <michael.boerger@gmail.com>
2014-02-28 14:06:51 -0500

    Finished

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var randomnum = Math.random();
+var randomnum = Math.floor((Math.random()*10)+1);
 var name = prompt('HELLO HUMAN FRIEND, WHAT IS YOUR NAME');
-var num = prompt('HELLO HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER');
-alert('YOU PICKED ' + num + ' COMPUTERS ARE SO USEFUL');
-alert('MY NUMBER WAS ' + randomnum + ' I AM NOT PROGRAMMED TO CARE IF YOU GUESSED IT');
+var num = prompt('HELLO HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER BETWEEN 1 AND 10');
+do
+{
+  alert('NO, BAD HUMAN');
+  if (num < randomnum){
+    alert("MY NUMBER IS LARGER BECAUSE COMPUTERS ARE POWERFUL");
+  }else {
+    alert("MY NUMBER IS SMALLER BECAUSE COMPUTERS ARE CRAFTY");
+  }
+  num = prompt('PICK THE CORRECT NUMBER OR REPORT FOR ');
+}
+while (num != randomnum);
+alert('VICTORY FOR THOSE WHO ARE VICTORIOUS');
