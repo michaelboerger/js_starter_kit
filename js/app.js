b0ec134fd9074174dbba4de5110b68fab86bdd54
Michael <michael.boerger@gmail.com>
2014-02-28 12:01:19 -0500

    Gets name and number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-alert('Hey find me to know where to put the javascript');
+var name = prompt('HELLO HUMAN FRIEND, WHAT IS YOUR NAME');
+var num = prompt('HELLO HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER');
+alert('YOU PICKED ' + num + ' COMPUTERS ARE SO USEFUL');
