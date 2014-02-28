ef8deced9568e67dd6bdc634e1f4cb3043912135
Michael <michael.boerger@gmail.com>
2014-02-28 17:46:44 -0500

    THIS IS PRETTY GREAT

--- js/app.js
+++ js/app.js
@@ -1,15 +1,40 @@
-var randomnum = Math.floor((Math.random()*10)+1);
+var playAgain = 'yes'
+var max = 10;
+var randomnum = Math.floor((Math.random()*max)+1);
 var name = prompt('HELLO HUMAN FRIEND, WHAT IS YOUR NAME');
-var num = prompt('HELLO HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER BETWEEN 1 AND 10');
+alert('FANTASTIC NAME ' + name.toUpperCase() + '!');
 do
 {
-  alert('NO, BAD HUMAN');
-  if (num < randomnum){
-    alert("MY NUMBER IS LARGER BECAUSE COMPUTERS ARE POWERFUL");
-  }else {
-    alert("MY NUMBER IS SMALLER BECAUSE COMPUTERS ARE CRAFTY");
+  if (playAgain  == 'yes') {
+    alert('TIME TO PLAY');
+  } else{
+    alert('YOU SAID ' + playAgain.toUpperCase() + ' AND I AM TAKING THAT AS A YES.');
   }
-  num = prompt('PICK THE CORRECT NUMBER OR REPORT FOR ');
+  var counter = 0;
+  var num = prompt('HUMAN FRIEND, ' + name.toUpperCase() +': PICK A NUMBER BETWEEN 1 AND ' + max + '!');
+  do
+  {
+    if (num == randomnum){
+      break;
+    }else if (num < randomnum){
+      alert("MY NUMBER IS LARGER BECAUSE COMPUTERS ARE POWERFUL");
+      counter++
+    }else {
+      alert("MY NUMBER IS SMALLER BECAUSE COMPUTERS ARE CRAFTY");
+      counter++
+    }
+    num = prompt('PICK THE CORRECT NUMBER');
+  }
+  while (num != randomnum);
+  counter++
+  alert('VICTORY FOR THOSE WHO ARE VICTORIOUS, IT TOOK YOU ' + counter + ' GUESSES!');
+    if (counter === 1){
+      alert('WHOA FIRST GUESS!');
+    }else if (counter < max/2){
+      alert('WELL PLAYED ' + name.toUpperCase());
+    }else {
+      alert(counter + ' GUESSES? BAD JOB ' + name.toUpperCase());
+    }
+  playAgain = prompt('PLAY AGAIN? YES OR NO')
 }
-while (num != randomnum);
-alert('VICTORY FOR THOSE WHO ARE VICTORIOUS');
+while (playAgain.toLowerCase() != 'no')
